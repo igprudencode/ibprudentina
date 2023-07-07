544903c8e16355ce8dcd904c6ddc82624748d9f4
Timóteo Reinheimer Piano <timoteopiano@gmail.com>
2023-07-07 14:51:07 -0300

    feat: add footer in the project

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return(
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <p>Copyright &copy; 2021</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
